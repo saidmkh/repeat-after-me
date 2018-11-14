ae46f39e91ddabfe9d089e2385a90cecec1f9cd9
Said <mokhammiedsaid@gmail.com>
2018-11-14 11:21:18 +0200

    fixed func

--- scripts.js
+++ scripts.js
@@ -22,9 +22,32 @@ compareArrays = (arr1, arr2) => {
   return true
 }
 
+handleShowColor = (block) => {
+  block.childNodes[1].classList.add('block_display')
+
+  setTimeout(() => {
+    block.childNodes[1].classList.remove('block_display')
+  }, 300);
+}
+
+disableBlocks = () => {
+  for (let i = 0; i < blocks.length; i++) {
+    blocks[i].classList.add('disabled')
+  }
+}
+
+enableBlocks = () => {
+  for (let i = 0; i < blocks.length; i++) {
+    blocks[i].classList.remove('disabled')
+  }
+}
+
 onload = addOnClick = () => {
   blocks.forEach(Blocks => Blocks.addEventListener('click', () => {
+    handleShowColor(Blocks)
+
     selected_colors.push(Blocks.id)
+
     if (selected_colors.length === rand_colors.length) {
       if (!compareArrays(selected_colors, rand_colors)) {
         selected_colors.length = 0
@@ -32,6 +55,8 @@ onload = addOnClick = () => {
         score.innerHTML = 1
         title.innerHTML = 'Try again'
         go_button.removeAttribute('disabled')
+
+        disableBlocks()
         return false
       }
 
@@ -39,13 +64,21 @@ onload = addOnClick = () => {
       score.innerHTML = +score.innerHTML + 1
       selected_colors.length = 0
       go_button.removeAttribute('disabled')
+
+      disableBlocks()
     }
   }, false))
+
+  disableBlocks()
 }
 
 go_button.onclick = getBlocks = () => {
+  disableBlocks()
+
   go_button.setAttribute('disabled', true)
+
   title.innerHTML = 'Repeat after me'
+
   rand_colors.push(COLOURS[Math.floor(Math.random() * COLOURS.length)])
 
   for (let i = 0; i < rand_colors.length; i++) {
@@ -58,6 +91,10 @@ go_button.onclick = getBlocks = () => {
       }, i * 800)
     })(i)
   }
+
+  setTimeout(() => {
+    enableBlocks()
+  }, rand_colors.length * 800);
 }
 
 
